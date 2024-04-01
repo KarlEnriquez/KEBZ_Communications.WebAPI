48a3ff32f42819e509ef799a8e23168d0224b34b
BryanZb <72435201+BryanZb@users.noreply.github.com>
2024-04-01 14:17:01 -0500

    Added entity files for User, Plan, Device, and UserPlan

---
+++ KEBZ_Communications.WebAPI/Entities/Device.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Filename: Device.cs
+ * Description: This file contains the definition of the Device entity class.
+ */
+
+using System.Numerics;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace KEBZ_Communications.WebAPI.Entities
+{
+    public class Device
+    {
+        public int DeviceId { get; set; }
+
+        [ForeignKey(nameof(User))]
+        public int UserId { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? IMEI { get; set; }
+        public string? Manufacturer { get; set; }
+        public string? Model { get; set; }
+
+        public User? User { get; set; }
+    }
+}
---
+++ KEBZ_Communications.WebAPI/Entities/Plan.cs
@@ -0,0 +1,24 @@
+﻿/*
+ * Filename: Plan.cs
+ * Description: This file contains the definition of the Plan entity class.
+ */
+
+namespace KEBZ_Communications.WebAPI.Entities
+{
+    public class Plan
+    {
+        public int PlanId { get; set; }
+        public string? PlanName { get; set; }
+        public string? PlanDescription { get; set; }
+        public decimal Price { get; set; }
+        public int DeviceLimit { get; set; }
+        public int DataLimit { get; set; }
+        public int MinuteLimit { get; set; }
+        public int TextLimit { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+        // Navigation property for many-to-many relationship with User
+        // 
+        public ICollection<UserPlan>? UserPlans { get; set; }
+    }
+}
