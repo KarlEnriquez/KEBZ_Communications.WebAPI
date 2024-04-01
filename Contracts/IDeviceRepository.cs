485fd79b1bc0184cb60cdd20e1283412f9f5740d
BryanZb <72435201+BryanZb@users.noreply.github.com>
2024-04-01 15:51:52 -0500

    Created Contracts project

---
+++ Contracts/IDeviceRepository.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Filename: IDeviceRepository.cs
+ * Description: This file contains the definition of the IDeviceRepository contracts class.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KEBZ_Communications.WebAPI.Entities;
+
+namespace Contracts
+{
+    public interface IDeviceRepository
+    {
+        IEnumerable<Device> GetAllDevices(bool trackChanges);
+        Device GetDevice(bool trackChanges);
+        void CreateDevice(Device device);
+        void DeleteDevice(Device device);
+    }
+}
---
+++ Contracts/IPlanRepository.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Filename: IPlanRepository.cs
+ * Description: This file contains the definition of the IPlanRepository contracts class.
+ */
+
+using KEBZ_Communications.WebAPI.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contracts
+{
+    public interface IPlanRepository
+    {
+        IEnumerable<Plan> GetAllPlans(bool trackChanges);
+        Plan GetPlan(bool trackChanges);
+        void CreatePlan(Plan plan);
+        void DeletePlan(Plan plan);
+    }
+}
